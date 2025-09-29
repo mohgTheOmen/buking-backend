f921c675247e578df1b6b88875fc653e3e9a5a9b
aiancovici <andreea.iancovici@booking.com>
2025-09-29 17:35:18 +0300

    Initial commit

---
+++ src/test/java/magis/mundi2025/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package magis.mundi2025.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
